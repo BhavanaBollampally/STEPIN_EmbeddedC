4c24e2fed21cc25288bb59df3e50b8c430b57e85
BhavanaBollampally <89590540+BhavanaBollampally@users.noreply.github.com>
2021-09-16 22:01:57 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <avr/io.h>
+
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+
+int main(){
+    activity_1();
+    activity_2();
+    activity_3();
+    return 0;
+}
+© 2021 GitHub, Inc.
