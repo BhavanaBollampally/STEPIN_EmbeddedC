03b7811c0e2ff579b4289c123d74150ea440e850
BhavanaBollampally <89590540+BhavanaBollampally@users.noreply.github.com>
2021-09-16 23:25:22 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -17,30 +17,30 @@ char activity3_loop (uint16_t var){
         OCR1A = 205; // 20% duty cycle
         temp_value = 20;
         _delay_ms(200);
-        uartWrite(temp_value);
+        uartwrite(temp_value);
     }
     else if (var<=509){
         OCR1A = 410; // 40% duty cycle
         temp_value = 25;
         _delay_ms(200);
-        uartWrite(temp_value);
+        uartwrite(temp_value);
     }
     else if ((var>=510) && (var<=709)){
         OCR1A = 717; // 70% duty cycle
         temp_value = 29;
         _delay_ms(200);
-        uartWrite(temp_value);
+        uartwrite(temp_value);
     }
     else if ((var>=710) && (var<=1024)){
         OCR1A = 973; // 95% duty cycle
         temp_value = 33;
         _delay_ms(200);
-        UartWrite(temp_value);
+        uartwrite(temp_value);
     }
     else{
         OCR1A = 0; // 0% duty cycle
         temp_value = 0;
-        uartWrite(temp_value);
+        uartwrite(temp_value);
     }
     return 0;
 }
