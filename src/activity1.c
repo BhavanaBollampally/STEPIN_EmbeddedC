01d1902536d897b8764a1e713373d67eb6599dd5
BhavanaBollampally <89590540+BhavanaBollampally@users.noreply.github.com>
2021-09-16 22:07:00 +0530

    Create activity1.c

---
+++ src/activity1.c
@@ -0,0 +1,27 @@
+#include "activity1.h"
+#include <util/delay.h>
+
+void pin_init(){
+    DDRB |= (1<<LED_PIN); // set pin for LED
+    DDRD &= ~(1<<HEATER_PIN); // clear bit
+    DDRC &= ~(1<<DRIVER_PIN); // clear bit
+
+    PORTD |= (1<<HEATER_PIN); // set bit
+    PORTC |= (1<<DRIVER_PIN); // set bit
+}
+
+void activity1_loop(){
+    if (!((PIND & (1<<HEATER_PIN)) || (PINC & (1<<DRIVER_PIN)))){
+        LED_PORT |= (1<<LED_PIN); // switch ON LED
+        _delay_ms(200);
+    }
+    else{
+        LED_PORT &= ~(1<<LED_PIN);
+        _delay_ms(200);
+}
+}
+
+void activity_1(){
+    pin_init();
+}
+
