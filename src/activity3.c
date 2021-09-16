a62270b4588575f1f126a78a5228def06c634154
BhavanaBollampally <89590540+BhavanaBollampally@users.noreply.github.com>
2021-09-16 23:45:42 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -13,37 +13,36 @@ void InitPwm(){
 char activity3_loop (uint16_t var) {
     char temp_value;
 
-    if ((var>=0) && var<=209)) {
+    if (var<=209) {
         OCR1A = 205; // 20% duty cycle
         temp_value = 20;
         _delay_ms(200);
         UartWrite(temp_value);
-      }
-    else if ((var>=210) && (var<=509) {
+     }
+    else if (var<=509) {
         OCR1A = 410; // 40% duty cycle
         temp_value = 25;
         _delay_ms(200);
-        UartWrite(temp_value);
-      }
+     }
     else if ((var>=510) && (var<=709)) {
         OCR1A = 717; // 70% duty cycle
         temp_value = 29;
         _delay_ms(200);
-        UartWrite(temp_value);
-      }
+     
+     }
     else if ((var>=710) && (var<=1024)) {
         OCR1A = 973; // 95% duty cycle
         temp_value = 33;
         _delay_ms(200);
-        UartWrite(temp_value);
+        
     }
     else {
         OCR1A = 0; // 0% duty cycle
         temp_value = 0;
-        UartWrite(temp_value);
-     }
+       
+    }
     return 0;
- }
+}
 
 void activity_3() {
     InitPwm();
